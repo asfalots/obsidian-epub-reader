fd33f41670dea90ba92d484d5b5b2ecf60c31dc0
Erwan Grooters <egr@n-side.com>
2025-07-07 12:15:43 +0200

    add setting to manage highlight colors

--- src/settings.ts
+++ src/settings.ts
@@ -1,14 +1,30 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
 import type EpubReaderPlugin from '../main';
 
+export interface HighlightConfig {
+	name: string;
+	color: string;
+	section: string;
+	template: string;
+}
+
 export interface EpubReaderSettings {
 	epubPropertyName: string;
 	progressPropertyName: string;
+	highlightConfigs: HighlightConfig[];
 }
 
 export const DEFAULT_SETTINGS: EpubReaderSettings = {
 	epubPropertyName: 'epub',
-	progressPropertyName: 'epub-cfi'
+	progressPropertyName: 'epub-cfi',
+	highlightConfigs: [
+		{
+			name: 'Important',
+			color: '#ffeb3b',
+			section: '## Highlights',
+			template: '> {{text}}'
+		}
+	]
 }
 
 export class EpubReaderSettingTab extends PluginSettingTab {
@@ -45,5 +61,97 @@ export class EpubReaderSettingTab extends PluginSettingTab {
 					this.plugin.settings.progressPropertyName = value;
 					await this.plugin.saveSettings();
 				}));
+
+		// Highlight Configurations
+		containerEl.createEl('h3', { text: 'Highlight Configurations' });
+		
+		this.plugin.settings.highlightConfigs.forEach((config, index) => {
+			this.addHighlightConfigSetting(containerEl, config, index);
+		});
+
+		// Add new highlight config button
+		new Setting(containerEl)
+			.addButton(button => button
+				.setButtonText('Add Highlight Config')
+				.setCta()
+				.onClick(async () => {
+					this.plugin.settings.highlightConfigs.push({
+						name: 'New Highlight',
+						color: '#ffeb3b',
+						section: '## Highlights',
+						template: '> {{text}}'
+					});
+					await this.plugin.saveSettings();
+					this.display(); // Refresh the settings view
+				}));
+	}
+
+	private addHighlightConfigSetting(containerEl: HTMLElement, config: HighlightConfig, index: number): void {
+		const configContainer = containerEl.createDiv('highlight-config');
+
+		// Header with name and delete button
+		const headerEl = configContainer.createDiv();
+		headerEl.style.display = 'flex';
+		headerEl.style.justifyContent = 'space-between';
+		headerEl.style.alignItems = 'center';
+		headerEl.style.marginBottom = '10px';
+		
+		headerEl.createEl('h4', { text: config.name || `Highlight Config ${index + 1}` });
+
+		const deleteButton = headerEl.createEl('button', { text: 'Delete' });
+		deleteButton.className = 'mod-warning';
+		deleteButton.onclick = async () => {
+			this.plugin.settings.highlightConfigs.splice(index, 1);
+			await this.plugin.saveSettings();
+			this.display(); // Refresh the settings view
+		};
+
+		// Name setting
+		new Setting(configContainer)
+			.setName('Name')
+			.setDesc('Display name for this highlight type')
+			.addText(text => text
+				.setValue(config.name)
+				.onChange(async (value) => {
+					config.name = value;
+					await this.plugin.saveSettings();
+				}));
+
+		// Color setting
+		new Setting(configContainer)
+			.setName('Color')
+			.setDesc('Highlight color (hex code)')
+			.addColorPicker(color => color
+				.setValue(config.color)
+				.onChange(async (value) => {
+					config.color = value;
+					await this.plugin.saveSettings();
+				}));
+
+		// Section setting
+		new Setting(configContainer)
+			.setName('Section')
+			.setDesc('Markdown heading where highlights will be added')
+			.addText(text => text
+				.setPlaceholder('## Highlights')
+				.setValue(config.section)
+				.onChange(async (value) => {
+					config.section = value;
+					await this.plugin.saveSettings();
+				}));
+
+		// Template setting
+		new Setting(configContainer)
+			.setName('Template')
+			.setDesc('Template for highlights. Use {{text}} for the highlighted text')
+			.addTextArea(text => {
+				text.setValue(config.template)
+					.onChange(async (value) => {
+						config.template = value;
+						await this.plugin.saveSettings();
+					});
+				text.inputEl.rows = 3;
+				return text;
+			});
 	}
 }
